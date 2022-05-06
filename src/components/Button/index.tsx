5f6a7bc683ed130ea87dbb670f5dfd57b53b8b64
shermondevelopment <victorshermon@yahoo.com>
2022-05-05 21:57:12 -0300

    creating page of signin

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import * as S from './styled'
+
+type PropsButton = {
+  children: React.ReactNode
+}
+
+const Button: React.FC<PropsButton> = ({ children }) => {
+  return (
+    <S.Button>
+      {children}
+    </S.Button>
+  )
+}
+
+export default Button
--- src/pages/signin/signin.tsx
+++ src/pages/signin/signin.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
+/* link */
+import { Link } from 'react-router-dom'
+
+/* Components */ 
+import Button from '../../components/Button'
+
+/* styled */
+import { PageSignin, PageSigninForm, PageSigninTitle } from './styled'
+
 const Signin: React.FC = () => {
   return (
-    <h3>Signin</h3>
+    <PageSignin>
+      <PageSigninForm>
+        <PageSigninTitle>MyWallet</PageSigninTitle>
+        <form className="form">
+          <input type="email" name="email" required placeholder="E-mail" />
+          <input type="password" name="password" required placeholder='Senha' />
+          <Button>Entrar</Button>
+        </form>
+        <Link to="/signup" className="signup-link">Primeira vez? Cadastre-se!</Link>
+      </PageSigninForm>
+    </PageSignin>
   )
 }
 
