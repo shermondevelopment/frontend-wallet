ff7f3db0de9238827e8c382879eee97900a29b34
shermondevelopment <victorshermon@yahoo.com>
2022-05-06 20:56:22 -0300

    creatng layout of list transaction

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,7 +12,44 @@ const Home: React.FC = () => {
         <img src="/assets/img/open.svg" alt="exit" />
       </S.PageHeader>
       <S.PageHomeMovimentions>
-        a
+        {/* <span className="not-movimentatios">Não há registros de entrada ou saida</span> */}
+        <S.ListContainerMoviemntation>
+          <S.ListMovimentatios>
+            <ul>
+              <li>
+                <S.ListOptions>
+                  <div className="list-info">
+                    <span className="list-date">30/01</span>
+                    <span className="list-text">Almoço mãe</span>
+                  </div>
+                  <span className="value-movimentation exit">39,90</span>
+                </S.ListOptions>
+              </li>
+              <li>
+                <S.ListOptions>
+                  <div className="list-info">
+                    <span className="list-date">20/05</span>
+                    <span className="list-text">Mercado</span>
+                  </div>
+                  <span className="value-movimentation exit">104,90</span>
+                </S.ListOptions>
+              </li>
+              <li>
+                <S.ListOptions>
+                  <div className="list-info">
+                    <span className="list-date">12/10</span>
+                    <span className="list-text">Farmácia</span>
+                  </div>
+                  <span className="value-movimentation entry">204,90</span>
+                </S.ListOptions>
+              </li>
+            </ul>
+          </S.ListMovimentatios>
+          <S.ListFooterInfo>
+            <span className="balance">Saldo</span>
+            <span className="money">2849,96</span>
+          </S.ListFooterInfo>
+        </S.ListContainerMoviemntation>
       </S.PageHomeMovimentions>
       <S.Transactions>
       <S.PageHomeNewTransaction>
--- src/pages/home/styled.tsx
+++ src/pages/home/styled.tsx
@@ -18,13 +18,27 @@ export const PageHomeTitle = styled.h1`
 `
 
 export const PageHomeMovimentions = styled.div`
-  width: 326px;
+  width: 100%;
   height: 446px;
+  padding: 23px 12px;
   background: #fff;
   border-radius: 5px;
+  display: fex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   margin-top: 22px;
-`
 
+  & > .not-movimentatios {
+    display: block;
+    width: 180px;
+    font-size: 20px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    text-align: center;
+    color: #868686;
+  }
+`
 
 
 export const Transactions = styled.div`
@@ -34,7 +48,7 @@ export const Transactions = styled.div`
 
 export const PageHomeNewTransaction = styled.div`
   width: 100%;
-  max-width: 155px;
+  max-width: 48%;
   height: 114px;
   background: #A328D6;
   border-radius: 5px;
@@ -55,6 +69,73 @@ export const PageHomeNewTransaction = styled.div`
     font-family: 'Raleway',sans-serif;
     font-weight: 700;
     color: #fff;
+    font-size: 17px;
+  }
+`
+
+export const ListContainerMoviemntation = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+export const ListMovimentatios = styled.div`
+  flex: 1;
+  
+  & > ul {
+    list-style: none;
+  }
+  & > ul > li {
+    margin-bottom: 20px;
+  }
+`
+
+export const ListOptions = styled.div`
+  display: flex;
+
+  & > .list-info > .list-date{
+    color: #C6C6C6;
+    font-size: 16px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+  }
+  & > .list-info > .list-text, & > .value-movimentation{
+    color: #000;
+    font-size: 16px;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    margin-left: 10px;
+  }
+
+  & > .value-movimentation {
+    flex: 1;
+    text-align: right;
+  }
+
+  & > .value-movimentation.exit {
+    color: #C70000;
+  }
+  & > .value-movimentation.entry {
+    color: #03AC00;
+  }
+`
+
+export const ListFooterInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  height: 40px;
+
+  & > .balance {
+    color: #000;
+    font-family: 'Raleway', sans-serif;
+    font-size: 17px;
+    font-weight: 700;
+  }
+  & > .money { 
+    color: #03AC00;
+    font-family: 'Raleway', sans-serif;
     font-size: 17px;
   }
 `
