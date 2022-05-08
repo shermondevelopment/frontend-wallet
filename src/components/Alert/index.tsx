289d7343fc5a1465c2703b45bae38513b10e4435
shermondevelopment <victorshermon@yahoo.com>
2022-05-08 09:36:52 -0300

    integration signin how api

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,38 @@
+import React, {ReactElement,useEffect,} from 'react'
+
+/* Icons */
+import { FiAlertCircle } from 'react-icons/fi'
+
+/* Styles */
+import * as S from './styled'
+
+/* PropsType */
+
+type PropsAlert = {
+  type: string
+  show: boolean
+  setshowAlert: any
+  children: React.ReactNode
+}
+
+const Alert: React.FC<PropsAlert> = ({
+  children,
+  type,
+  show,
+  setshowAlert,
+}): ReactElement => {
+  useEffect(() => {
+    show &&
+      setTimeout(() => {
+        setshowAlert({ show: false })
+      }, 4000)
+  }, [show])
+
+  return (
+    <S.Alert type={type} show={show}>
+      {type === 'error' && <FiAlertCircle size={22} />} <span>{children}</span>
+    </S.Alert>
+  )
+}
+
+export default Alert
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -3,6 +3,12 @@ import React, { useState } from 'react'
 /* link */
 import { Link } from 'react-router-dom'
 
+/* axios */
+import api from '../../services/api'
+
+/* alert */
+import Alert from '../../components/Alert'
+
 /* Components */ 
 import Button from '../../components/Button'
 
@@ -13,19 +19,34 @@ const Signin: React.FC = () => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [message, setMessage] = useState({ show: false, alert: '' })
 
-  const handleSignin = (e: React.FormEvent<HTMLFormElement>) => {
-    alert('ola')
+
+  const handleSignin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    try {
+      const userLogged = await api.post('/signin', { email, password })
+      localStorage.setItem('token', userLogged.data.token)
+    } catch (error: any) {
+      setMessage({
+        alert: error?.response?.data?.error,
+        show: true,
+      })
+      setEmail('')
+      setPassword('')
+    }
   }
 
   return (
     <PageSignin>
+      <Alert show={message.show} type="error" setshowAlert={setMessage}>
+       {message.alert}
+      </Alert>
       <PageSigninForm>
         <PageSigninTitle>MyWallet</PageSigninTitle>
         <form className="form" onSubmit={handleSignin}>
-          <input type="email" name="email" required placeholder="E-mail" onChange={e => setEmail(e.currentTarget.value)} />
-          <input type="password" name="password" required placeholder='Senha' onChange={e => setPassword(e.currentTarget.value)} />
+          <input type="email" name="email" required placeholder="E-mail" value={email} onChange={e => setEmail(e.currentTarget.value)} />
+          <input type="password" name="password" required placeholder="Senha" value={password} onChange={e => setPassword(e.currentTarget.value)} />
           <Button>Entrar</Button>
         </form>
         <Link to="/signup" className="signup-link">Primeira vez? Cadastre-se!</Link>
